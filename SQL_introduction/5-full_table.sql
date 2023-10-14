2d9278f15c5341abb5481e29f71b6048b399234f
dndegeber <dorandege@gmaiil.com>
2023-10-14 10:02:31 -0700

    Full description correct

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -4,6 +4,6 @@
 -- The user is prompted to enter the password for authentication
 -- The query uses the information_schema database to retrieve
 -- table information without using DESCRIBE or EXPLAIN
-SELECT TABLE_NAME, CREATE_TABLE
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_test_db_4' AND TABLE_NAME = 'first_table';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
