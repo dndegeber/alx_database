49e909565a4396d77e352b2ca3af264dd91852c6
dndegeber <dorandege@gmaiil.com>
2023-10-14 09:40:41 -0700

    First add

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- This SQL script inserts a new row into the table first_table in the specified database.
+
+-- The user is prompted to enter the password for authentication
+INSERT INTO hbtn_test_db_7.first_table (id, name)
+VALUES (89, 'Holberton School');
