4f15037f7878b67da59141120c21c44c9dbfae4d
dndegeber <dorandege@gmaiil.com>
2023-10-14 08:38:47 -0700

    List databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,1 +1,6 @@
+
+
+-- This SQL script lists all databases present on the MySQL server.
+
+-- Show the list of databases
 SHOW DATABASES;
--- SQL_introduction/README.md
+++ SQL_introduction/README.md
@@ -1,1 +1,1 @@
-This is my readme on SQL introduction
+SQL - Introduction
