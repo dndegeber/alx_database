4390d54a074064dfc64ed0e063a5584b2f9bf7c1
dndegeber <dorandege@gmaiil.com>
2023-10-14 09:35:46 -0700

    List all in table

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- This SQL script lists all rows of the table first_table from the specified database.
+
+-- The user is prompted to enter the password for authentication
+SELECT *
+FROM hbtn_test_db_6.first_table;
