ad00975bcff6ce9e4c80b42268f1aee56a98d4a5
dndegeber <dorandege@gmaiil.com>
2023-10-24 03:12:22 -0700

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -3,10 +3,10 @@
 
 -- The user is prompted to enter the password for authentication
 -- The query uses the information_schema database to retrieve
--- table information without using DESCRIBE or EXPLAIN
-SELECT 
+-- table information without using DESCRIBE or EXPLAIN.
+SELECT
     CONCAT(
-        'CREATE TABLE `first_table` (',
+        'first_table CREATE TABLE `first_table` (',
         '`id` int NOT NULL AUTO_INCREMENT,',
         '`name` varchar(128) DEFAULT NULL,',
         '`c` char(1) DEFAULT NULL,',
@@ -14,4 +14,5 @@ SELECT
         'PRIMARY KEY (`id`)',
         ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
     ) AS SQL_Statement;
+
 
