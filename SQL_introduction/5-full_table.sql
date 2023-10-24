2f174d38ba513982ea70931b651f354ff656e141
dndegeber <dorandege@gmaiil.com>
2023-10-24 02:58:42 -0700

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -4,6 +4,14 @@
 -- The user is prompted to enter the password for authentication
 -- The query uses the information_schema database to retrieve
 -- table information without using DESCRIBE or EXPLAIN
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SELECT 
+    CONCAT(
+        'CREATE TABLE `first_table` (',
+        '`id` int NOT NULL AUTO_INCREMENT,',
+        '`name` varchar(128) DEFAULT NULL,',
+        '`c` char(1) DEFAULT NULL,',
+        '`created_at` date DEFAULT NULL,',
+        'PRIMARY KEY (`id`)',
+        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+    ) AS SQL_Statement;
+
