24a1c6bf5bdffa9a34c6ece66631cd3168998d89
dndegeber <dorandege@gmaiil.com>
2023-10-24 03:28:14 -0700

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -7,12 +7,12 @@
 SELECT
     CONCAT(
         'first_table CREATE TABLE `first_table` (',
-        '`id` int NOT NULL AUTO_INCREMENT,',
-        '`name` varchar(128) DEFAULT NULL,',
-        '`c` char(1) DEFAULT NULL,',
-        '`created_at` date DEFAULT NULL,',
-        'PRIMARY KEY (`id`)',
-        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+        '\n  `id` int NOT NULL AUTO_INCREMENT,',
+        '\n  `name` varchar(128) DEFAULT NULL,',
+        '\n  `c` char(1) DEFAULT NULL,',
+        '\n  `created_at` date DEFAULT NULL,',
+        '\n  PRIMARY KEY (`id`)',
+        '\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
     ) AS SQL_Statement;
 
 
